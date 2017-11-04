b164de576e67531284834efbd686d18758b4caf6
Chris Jordan <jordanchriss@gmail.com>
2017-11-04 10:44:47 -0400

    added clappr, support for setting src from url, keybindings for jumping forward in time, updating url hash  with time which is read on reload

---
+++ script.js
@@ -0,0 +1,28 @@
+const query = new URLSearchParams(window.location.search);
+const src = query.get('src').replace(/\/$/, '');
+
+console.log(src);
+
+const player = new Clappr.Player({source: src, parentId: "#player"});
+
+const timeControls = {
+    j: 5,
+    k: 120,
+    l: 17 * 20
+};
+
+document.addEventListener('keypress', (evt) => {
+    const timeChange = timeControls[evt.key];
+
+    if (timeChange) {
+        player.seek(player.getCurrentTime() + timeChange);
+    }
+});
+
+var updateUrlTime = () => {
+    window.location.hash = `t=${player.getCurrentTime()}`;
+}
+
+player.on(Clappr.Events.PLAYER_PAUSE, updateUrlTime);
+
+setInterval(updateUrlTime, 1 * 1000);
